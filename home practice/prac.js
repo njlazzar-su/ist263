10d5c0b64575775e935f96801e0036f4fbb86a38
njlazzar-su <nicklazzari15@gmail.com>
2022-02-23 15:43:52 -0500

    pushing changes
    websites

---
+++ home practice/prac.js
@@ -0,0 +1,33 @@
+console.log('Hello World'); //This is an inline comment
+
+/* this is 
+
+a
+
+multiline comment */
+
+//Three ways to declare variables
+
+var myName = "Nick" //Var declaration can be used throughout the entire code
+
+let ourName = "Pike" //Let can only be used within the scope in which its declared
+
+const pi = 3.14 //Const can never change
+
+//Declaring variables
+
+var a; //Here we are simply declaring a variable
+
+var b = 2; //Here we declare the variable "b" and then assign a value of 2
+
+a = 7; //Here we assign 7 to "a" we dont have to declare since it was already done
+
+a = b; //Whatever the value of "b" is, is now "a"
+
+console.log(a);
+
+//Adding
+
+
+
+
